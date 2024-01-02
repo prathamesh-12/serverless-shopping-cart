4e04077a4c82c9bcdfec802d8af0d3bd5587ac37
prathamesh-12 <53848995+mansiHaldankar@users.noreply.github.com>
2024-01-03 00:13:48 +0530

    First Commit

--- lib/sqs.ts
+++ lib/sqs.ts
@@ -7,13 +7,3 @@ export class ServerlessSQS extends Construct {
         
     }
 }
-ord for 'https://prathamesh-12@github.com': 
-remote: Support for password authentication was removed on August 13, 2021.
-remote: Please see https://docs.github.com/en/get-started/getting-started-with-git/about-remote-repositories#cloning-with-https-urls for information on currently recommended modes of authentication.
-fatal: Authentication failed for 'https://github.com/prathamesh-12/serverless-shopping-cart.git/'
-Mansis-MacBook
-
-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
-
-git remote set-url origin https://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@github.com/prathamesh-12/serverless-shopping-cart.git
-git remote set-url origin https://prathamesh-12@github.com/prathamesh-12/serverless-shopping-cart.git
